24cb17845321fb955d0d22cf0c53095064d41731
Winicios Meirellis Mauri <winicios.mauri@oncorte.com.br>
2022-05-30 21:32:02 -0300

    :tada criando componente toggle

---
+++ src/components/Toggle/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { Container, ToggleLabel, ToggleSelector } from './style'
+
+const Toggle: React.FC = () => {
+  return (
+    <Container>
+      <ToggleLabel>Light</ToggleLabel>
+      <ToggleSelector
+        uncheckedIcon={false}
+        checkedIcon={false}
+        onChange={() => console.log('mudou')}
+        checked
+      />
+      <ToggleLabel>Dark</ToggleLabel>
+    </Container>
+  )
+}
+
+export default Toggle
