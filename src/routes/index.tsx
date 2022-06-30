213762b8ef00fe21f4cb2d0275873a6a09358673
Winicios Meirellis Mauri <winicios.mauri@oncorte.com.br>
2022-06-30 20:52:37 -0300

    fix: creating the login screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
-import App from './app.routes'
+// import App from './app.routes'
 
-const Routes: React.FC = () => <App />
+import Auth from './auth.routes'
+
+const Routes: React.FC = () => (
+  <BrowserRouter>
+    <Auth />
+  </BrowserRouter>
+)
 
 export default Routes
