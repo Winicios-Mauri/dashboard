f372c30711de1e8bbf25a6fc407dd98c6456d8aa
Winicios Meirellis Mauri <winicios.mauri@oncorte.com.br>
2022-07-02 15:39:19 -0300

    fix: applying animation to components

--- src/components/BarChartBox/styles.ts
+++ src/components/BarChartBox/styles.ts
@@ -1,8 +1,22 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 interface ILengendProps {
   color: string
 }
+
+const animate = keyframes`
+  0%{
+    transform: translateX(100px);
+    opacity: 0;
+  }
+  50%{
+    opacity: .3;
+  }
+  100%{
+    transform: translateX(0px);
+    opacity: 1;
+  }
+`
 
 export const Container = styled.div`
   width: 48%;
@@ -14,6 +28,8 @@ export const Container = styled.div`
   color: ${props => props.theme.colors.white};
 
   border-radius: 7px;
+
+  animation: ${animate} 0.5s;
 
   @media (max-width: 1200px) {
     display: flex;
